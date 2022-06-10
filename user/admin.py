793cd46a6a1d2dc382ce1274cb26397c002ee526
JerryKim <ecec1023@gmail.com>
2022-06-10 11:23:19 +0900

    Group 모델 admin에서 삭제 / User 모델은 나중에 커스텀 모델로 중복 없애고 커스텀 모델을 사용할 예정 / HobbyAdmin으로 admin에서 보여지는 화면 커스텀

--- user/admin.py
+++ user/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-from .models import Hobby, User, UserProfile
+from user.models import User as UserModel
+from user.models import UserProfile as UserProfileModel
+from user.models import Hobby as HobbyModel
 
 # Register your models here.
 
+#unregister group model
+admin.site.unregister(Group)
+
+class HobbyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
 # admin.py
-admin.site.register(User)
-admin.site.register(UserProfile)
-admin.site.register(Hobby)
+admin.site.register(UserModel)
+admin.site.register(UserProfileModel)
+admin.site.register(HobbyModel, HobbyAdmin)
