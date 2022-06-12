8989182145bb60f736947fdee457173d50852767
JerryKim <ecec1023@gmail.com>
2022-06-13 02:12:09 +0900

    추가 수정 - 로그인 기능 추가 , 취미가 같은 유저찾는 코드 추가 / admin에서 프로필 설정에 hobby가 빠지는 버그 발생

--- user/views.py
+++ user/views.py
@@ -5,8 +5,23 @@ from django.shortcuts import get_object_or_404, render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import permissions
+from rest_framework import status
 
-from user.models import User
+from user.models import Hobby, User
+from django.contrib.auth import authenticate, login
+from django.db.models import F
+class UserApiView(APIView):
+    # 로그인
+    def post(self, request):
+        username = request.data.get('username', '')
+        password = request.data.get('password', '')
+
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return Response({"error": "존재하지 않는 계정이거나 패스워드가 일치하지 않습니다."}, status=status.HTTP_401_UNAUTHORIZED)
+
+        login(request, user)
+        return Response({"message": "로그인 성공!!"}, status=status.HTTP_200_OK)
 
 class MyGoodPermission(permissions.BasePermission):
     """
@@ -22,22 +37,53 @@ class MyGoodPermission(permissions.BasePermission):
         return 
 
 class UserView(APIView): # CBV 방식
-    permission_classes = [MyGoodPermission]
-    # permission_classes = [permissions.AllowAny] # 누구나 view 조회 가능
+    # permission_classes = [MyGoodPermission]
+    permission_classes = [permissions.AllowAny] # 누구나 view 조회 가능
     # permission_classes = [permissions.IsAdminUser] # admin만 view 조회 가능
     # permission_classes = [permissions.IsAuthenticated] # 로그인 된 사용자만 view 조회 가능
 
-    def get(self, request):
-        return Response({'message': 'get method!!'})
-        
-    def post(self, request):
-        return Response({'message': 'post method!!'})
+    def get_same_hobby(self, request):
+        user = request.user
+        # user = User.objects.get(id=1)
+        # print(user)
 
-    def put(self, request):
-        return Response({'message': 'put method!!'})
 
-    def delete(self, request):
-        return Response({'message': 'delete method!!'})
+        hobbys = user.userprofile.hobby.all()
+        for hobby in hobbys:
+            # exclde : 매칭 된 쿼리만 제외, filter와 반대
+		    # annotate : 필드 이름을 변경해주기 위해 사용, 이외에도 원하는 필드를 추가하는 등 다양하게 활용 가능
+		    # values / values_list : 지정한 필드만 리턴 할 수 있음. values는 dict로 return, values_list는 tuple로 ruturn
+		    # F() : 객체에 해당되는 쿼리를 생성함
+            hobby_members = hobby.userprofile_set.exclude(user=user).annotate(username=F('user__username')).values_list('username', flat=True)
+            # hobby_members = list(hobby_members) # 리스트로 보려면 리스트화
+            print(f"hobby : {hobby.name} / hobby members : {hobby_members}")
+        return Response({})
+# result print
+"""
+hobby : 산책 / hobby members : ['user1']
+hobby : 음악감상 / hobby members : ['user1', 'user2']
+hobby : 스쿠버다이빙 / hobby members : ['user2']
+hobby : 여행 / hobby members : ['user2']
+"""
+
+    # def get(self, request):
+    #     try:
+    #         # hobby_list = Hobby.objects.all()
+    #         hobby_list = Hobby.objects.filter(id__gt=5)[0]
+    #         print(hobby_list)
+    #     except:
+    #         return Response({"message": "오브젝트가 존재하지 않습니다."},status=status.HTTP_400_BAD_REQUEST)
+
+    #     return Response({'message': 'get method!!'})
+
+    # def post(self, request):
+    #     return Response({'message': 'post method!!'})
+
+    # def put(self, request):
+    #     return Response({'message': 'put method!!'})
+
+    # def delete(self, request):
+    #     return Response({'message': 'delete method!!'})
 
 
 class ListUsers(APIView):
