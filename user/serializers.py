85cd7c215cebb71ce610b97fedf8a8ca78cfc311
JerryKim <ecec1023@gmail.com>
2022-06-17 12:22:05 +0900

    시리얼라이저 필드에 다른 시리얼라이저 data 껴넣기 + 나랑 같은 취미 가진 사람들 역참조 dir()로 찍어봐서 찾아서 구하는 로직 구현

--- user/serializers.py
+++ user/serializers.py
@@ -3,12 +3,38 @@ from user.models import User as UserModel
 from user.models import UserProfile as UserProfileModel
 from user.models import Hobby as HobbyModel
 
+class HobbySerializer(serializers.ModelSerializer):
+    same_hobby_users = serializers.SerializerMethodField()
+    def get_same_hobby_users(self, obj):
+        # print(f"{obj} / {type(obj)}")
+        user_list = []
+        # print(dir(obj))
+        # print(obj.userprofile_set.all())
+        for userprofile in obj.userprofile_set.all():
+            user_list.append(userprofile.user.fullname)
+
+
+        return user_list
+    class Meta:
+        model = HobbyModel
+        fields = ["name", "same_hobby_users"]
+        # fields = "__all__"
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    hobby = HobbySerializer(many=True)
+    class Meta:
+        model = UserProfileModel
+        fields = ["introduction", "birthday", "age", "hobby"]
+        # fields = "__all__"
+
 class UserSerializer(serializers.ModelSerializer):
+    userprofile = UserProfileSerializer()
+
     class Meta:
         # serializer에 사용될 model, field지정
         model = UserModel
         # 모든 필드를 사용하고 싶을 경우 fields = "__all__"로 사용
-        fields = ["username", "fullname", "email", "password", "join_date"]  
+        fields = ["username", "fullname", "email", "password", "join_date", "userprofile"]  
         exrta_kwargs = {
             'password': {'write_only': True},
         } # password를 write_only 로 설정하면 data 안에 password는 남아있지만 정보를 누군가 보는 행위는 못함.
