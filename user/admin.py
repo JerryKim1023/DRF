9d2498bd16b0de5bc10e9a8675911ba84911ccaf
JerryKim <ecec1023@gmail.com>
2022-06-18 01:33:00 +0900

    수정 - admin page에서 패스워드 수정 안 되던 이슈 UserAdmin을 상속받아서 처리 / 이 방법이 아니면 form 변경해서 처리(form에 set_password 함수 추가)

--- user/admin.py
+++ user/admin.py
@@ -5,6 +5,8 @@ from user.models import User as UserModel
 from user.models import UserProfile as UserProfileModel
 from user.models import Hobby as HobbyModel
 
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 #unregister group model
 admin.site.unregister(Group)
 
@@ -13,14 +15,15 @@ admin.site.unregister(Group)
 class UserProfileInline(admin.StackedInline): # 세로로 뿌려줌
     model = UserProfileModel
 
+
     # def formfield_for_manytomany(self, db_field, request, **kwargs): # 내가 원하는 필드에서 보여주고 싶은 것만 보여주게 예외처리하기 / 폼필드 설정
     #     if db_field.name == 'hobby':
     #         kwargs['queryset'] = HobbyModel.objects.filter(id__lte=7)
 
     #     return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
-
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
+    filter_horizontal = []
     list_display = ('id', 'username', 'fullname')
     list_display_links = ('id', 'username', )
     list_filter = ('fullname', )
@@ -28,13 +31,22 @@ class UserAdmin(admin.ModelAdmin):
     readonly_fields = ('username', 'join_date', )
 
     fieldsets = (
-        ("info", {'fields': ('username', 'fullname', 'join_date')}),
+        ("info", {'fields': ('username', "password", 'fullname', 'join_date')}),
         ('permissions', {'fields': ('is_admin', 'is_active', )}),
     )
 
     inlines = (
             UserProfileInline,
         )
+
+    # def has_add_permission(self, request, obj=None): # 추가 권한
+    #     return False
+
+    # def has_delete_permission(self, request, obj=None): # 삭제 권한
+    #     return False
+
+    # def has_change_permission(self, request, obj=None): # 수정 권한
+    #     return False
 class HobbyAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
 
