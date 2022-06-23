c1292eee2869b1767d4a1d2e81afeaff2aef1a85
JerryKim <ecec1023@gmail.com>
2022-06-23 13:19:20 +0900

    Add a url path
    In class view some methods needed <obj_id>, so I contained  <obj_id> in the added path.

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.UserApiView.as_view(), name="user_api_view"), # signup, login 등 user의 기본적인 api담당
+	path('<obj_id>/', views.UserApiView.as_view(), name="user_api_view"), # signup, login 등 user의 기본적인 api담당
 	path('view/', views.UserView.as_view(), name="user_view"), # 같은 취미의 users를 찾는 등 추가기능 담당
 	path('list/', views.ListUsers.as_view(), name="login_user_view"),
 ]
